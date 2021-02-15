86c02b4edbeed468779b4945536de0bfd3c44bff
Riza Fahmi <rizafahmi@gmail.com>
2021-02-15 11:06:33 +0700

    Migrasi dari static site ke react :rocket:

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
 import React from "react";
 
 import Header from "./components/Header.js";
+import Hero from "./components/Hero.js";
+import Browse from "./components/Browse.js";
+import Arrived from "./components/Arrived.js";
+import Clients from "./components/Clients.js";
+import Modal from "./components/Modal.js";
 import Footer from "./components/Footer.js";
 
 function App() {
+  const [showModal, setShowModal] = React.useState(false);
+
+  function handleShowModal(event) {
+    setShowModal(!showModal);
+  }
   return (
     <>
       <Header />
+      <Hero handleShowModal={handleShowModal} />
+      <Browse />
+      <Arrived />
+      <Clients />
       <Footer />
+      {showModal && <Modal handleShowModal={handleShowModal} />}
     </>
   );
 }
