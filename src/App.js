cfc766bb02894e318d28ff4780074d5c7b41fc9e
Riza Fahmi <rizafahmi@gmail.com>
2021-02-23 16:38:22 +0700

    Finish connect to API

--- src/App.js
+++ src/App.js
@@ -10,16 +10,33 @@ import Footer from "./components/Footer.js";
 
 function App() {
   const [showModal, setShowModal] = React.useState(false);
+  const [items, setItems] = React.useState([]);
 
   function handleShowModal(event) {
     setShowModal(!showModal);
   }
+  React.useEffect(function () {
+    (async function () {
+      const response = await fetch(
+        "https://prod-qore-app.qorebase.io/8ySrll0jkMkSJVk/allItems/rows?limit=7&offset=0&$order=asc",
+        {
+          headers: {
+            "Content-Type": "application/json",
+            accept: "application/json",
+            "x-api-key": process.env.REACT_APP_APIKEY,
+          },
+        }
+      );
+      const { nodes } = await response.json();
+      setItems(nodes);
+    })();
+  }, []);
   return (
     <>
       <Header />
       <Hero handleShowModal={handleShowModal} />
       <Browse />
-      <Arrived />
+      <Arrived items={items} />
       <Clients />
       <Footer />
       {showModal && <Modal handleShowModal={handleShowModal} />}
