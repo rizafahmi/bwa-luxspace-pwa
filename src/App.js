9c42c40e228ad7531490641966ab9ec33fc5a2b0
Riza Fahmi <rizafahmi@gmail.com>
2021-02-23 23:38:33 +0700

    Finish offline status

--- src/App.js
+++ src/App.js
@@ -7,32 +7,51 @@ import Arrived from "./components/Arrived.js";
 import Clients from "./components/Clients.js";
 import Modal from "./components/Modal.js";
 import Footer from "./components/Footer.js";
+import Offline from "./components/Offline.js";
 
 function App() {
   const [showModal, setShowModal] = React.useState(false);
   const [items, setItems] = React.useState([]);
+  const [offlineStatus, setOfflineStatus] = React.useState(!navigator.onLine);
 
+  function handleOfflineStatus() {
+    console.log(!navigator.onLine);
+    setOfflineStatus(!navigator.onLine);
+  }
   function handleShowModal(event) {
     setShowModal(!showModal);
   }
-  React.useEffect(function () {
-    (async function () {
-      const response = await fetch(
-        "https://prod-qore-app.qorebase.io/8ySrll0jkMkSJVk/allItems/rows?limit=7&offset=0&$order=asc",
-        {
-          headers: {
-            "Content-Type": "application/json",
-            accept: "application/json",
-            "x-api-key": process.env.REACT_APP_APIKEY,
-          },
-        }
-      );
-      const { nodes } = await response.json();
-      setItems(nodes);
-    })();
-  }, []);
+  React.useEffect(
+    function () {
+      (async function () {
+        const response = await fetch(
+          "https://prod-qore-app.qorebase.io/8ySrll0jkMkSJVk/allItems/rows?limit=7&offset=0&$order=asc",
+          {
+            headers: {
+              "Content-Type": "application/json",
+              accept: "application/json",
+              "x-api-key": process.env.REACT_APP_APIKEY,
+            },
+          }
+        );
+        const { nodes } = await response.json();
+        setItems(nodes);
+      })();
+
+      handleOfflineStatus();
+      window.addEventListener("online", handleOfflineStatus);
+      window.addEventListener("offline", handleOfflineStatus);
+
+      return function cleanup() {
+        window.removeEventListener("online", handleOfflineStatus);
+        window.removeEventListener("offline", handleOfflineStatus);
+      };
+    },
+    [offlineStatus]
+  );
   return (
     <>
+      {offlineStatus && <Offline />}
       <Header />
       <Hero handleShowModal={handleShowModal} />
       <Browse />
