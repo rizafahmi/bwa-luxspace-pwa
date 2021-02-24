90672f54f763e130e275de7d8a45dc5a3774d02d
Riza Fahmi <rizafahmi@gmail.com>
2021-02-24 23:30:19 +0700

    Push notif. WIP

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,5 +1,34 @@
 import Header from "../components/Header.js";
 
+function urlB64ToUint8Array(base64String) {
+  const padding = "=".repeat((4 - (base64String.length % 4)) % 4);
+  const base64 = (base64String + padding)
+    .replace(/\-/g, "+")
+    .replace(/_/g, "/");
+
+  const rawData = window.atob(base64);
+  const outputArray = new Uint8Array(rawData.length);
+
+  for (let i = 0; i < rawData.length; ++i) {
+    outputArray[i] = rawData.charCodeAt(i);
+  }
+  return outputArray;
+}
+
+async function subscribe() {
+  const key =
+    "BD1z4z06UTMuFMu6rk1KOlqtw4DBsHUNSMVN_3cKPnzuOUqowN13ocmh_vjJLkxvxREl21zY85OQt53ZSl9bfW8";
+  try {
+    const sub = await global.registration.pushManager.subscribe({
+      userVisibleOnly: true,
+      applicationServerKey: urlB64ToUint8Array(key),
+    });
+    console.log("Subscribed!");
+  } catch (error) {
+    console.error("Cannot subscribe.");
+  }
+}
+
 function Profile() {
   return (
     <>
@@ -17,6 +46,7 @@ function Profile() {
               <button
                 type="button"
                 className="inline-flex items-center justify-center mr-6 px-4 py-2 border border-transparent font-medium rounded-md text-teal-700 bg-teal-100 hover:bg-teal-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-teal-500 sm:text-sm"
+                onClick={subscribe}
               >
                 Subscribe to Notification
               </button>
--- src/service-worker.js
+++ src/service-worker.js
@@ -120,3 +120,11 @@ self.addEventListener("message", (event) => {
 });
 
 // Any other custom service worker logic can go here.
+self.addEventListerner("push", function (event) {
+  event.waitUntil(
+    self.registration.showNotification("LuxSpace", {
+      icon: "./icon-120.png",
+      body: event.data.text(),
+    })
+  );
+});
